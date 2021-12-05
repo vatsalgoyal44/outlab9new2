7ad1df824ba175b6c9ada7251369c9dc135bfd35
SanyamSaxena <sanyamsaxena2002@gmail.com>
2021-12-06 02:37:03 +0530

    recyclerview_assignments

---
+++ app/src/main/java/com/example/calendarapp/Add_Event.java
@@ -0,0 +1,14 @@
+package com.example.calendarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
